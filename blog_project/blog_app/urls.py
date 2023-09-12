f29fc50be4c454bc852c40170c4c8bc48a635be9
Leejoohyeong1 <tkdrb4807@gmail.com>
2023-09-12 18:02:44 +0900

    django view.py 초안 작성

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(r'', views.index, name='index'),
+    path(r'login', views.login, name='login'),
+    path(r'post', views.post, name='post'),
+    path(r'post/<int:board_id>', views.postDtl, name='postDtl'),
+    path(r'post/write', views.postWrite, name='postWrite'),
 ]
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    if request.method == "GET":
+        # TODO 로그인 페이지 로 이동
+        # return render(request, '로')
+        print(1)
+    elif request.method == "POST":
+        # TODO 로그인시 메인페이로 이동
+        return redirect('/')
+
+
+
+    # 목록
+def post(request):
+    topic = request.GET['topic']
+
+    if(topic is None):
+        return render(request, 'postWrite.html')
+        print(1)
+    else:
+        print(1)
+    
+
+    # 상세
+def postDtl(request,board_id):
+    print(1)
+
+
+
+def postWrite(request):
+    if request.method == "POST":
+        # 글쓰기
+        # code...
+        return render(request, 'postWrite.html')
+    elif request.method == "PUT":
+        # 글 수정
+        # code...
+        return render(request, 'postWrite.html')
+    elif request.method == "DELETE":
+        # 글 삭제
+        # code...
+        return redirect('/')
+    else: 
+        return render(request, 'postWrite.html')
+
+    
