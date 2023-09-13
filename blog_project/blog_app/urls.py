47903b0737d79ce85ed4ba2a2af7a3d1912a53ed
park <wjwjsk12345@gmail.com>
2023-09-13 12:29:06 +0900

    connect index - login

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path(r'', views.index, name='index'),
-    path(r'login', views.login, name='login'),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(r'post', views.post, name='post'),
     path(r'post/<int:board_id>', views.postDtl, name='postDtl'),
-    path(r'post/write', views.postWrite, name='postWrite'),
+    path(r'post/write', views.post_write, name='post_write'),
 ]
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,53 +1,55 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from .models import Topic, Board
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    topics = Topic.objects.all()
+    most_view_post = Board.objects.order_by('-viewcount').values().first()
+    posts=Board.objects.all()
+    return render(request, "index.html", {"topics": topics, "most_view_post": most_view_post, "posts": posts})
 
 
-def login(request):
-    if request.method == "GET":
-        # TODO 로그인 페이지 로 이동
-        # return render(request, '로')
-        print(1)
-    elif request.method == "POST":
-        # TODO 로그인시 메인페이로 이동
-        return redirect('/')
+# def login(request):
+#     if request.method == "GET":
+#         # TODO 로그인 페이지 로 이동
+#         return render(request, 'login.html')
+#         print(1)
+#     elif request.method == "POST":
+#         # TODO 로그인시 메인페이로 이동
+#         return redirect("/")
+
+#     # 목록
 
 
-
-    # 목록
 def post(request):
-    topic = request.GET['topic']
+    topic = request.GET["topic"]
 
-    if(topic is None):
-        return render(request, 'postWrite.html')
+    if topic is None:
+        return render(request, "postWrite.html")
         print(1)
     else:
         print(1)
-    
 
     # 상세
-def postDtl(request,board_id):
+
+
+def postDtl(request, board_id):
     print(1)
 
 
-
-def postWrite(request):
+def post_write(request):
     if request.method == "POST":
         # 글쓰기
         # code...
-        return render(request, 'postWrite.html')
+        return render(request, "postWrite.html")
     elif request.method == "PUT":
         # 글 수정
         # code...
-        return render(request, 'postWrite.html')
+        return render(request, "postWrite.html")
     elif request.method == "DELETE":
         # 글 삭제
         # code...
-        return redirect('/')
-    else: 
-        return render(request, 'postWrite.html')
-
-    
+        return redirect("/")
+    else:
+        return render(request, "postWrite.html")
