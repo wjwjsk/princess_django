ecbdc71abc469cd61ff7a72e47bc2feb900adc30
park <wjwjsk12345@gmail.com>
2023-09-14 17:27:54 +0900

    update index.html and navbar.html

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -11,10 +11,10 @@ router.register(r"Board", BoardViewset)
 
 urlpatterns = [
     path(r"", views.index, name="index"),
-    path(r'login/', CustomLoginView.as_view(template_name='registration/login.html'), name='login'),
+    path(r"login/", CustomLoginView.as_view(template_name="registration/login.html"), name="login"),
     path(r"logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(r"post", views.post, name="post"),
     path(r"post/<int:board_id>", views.postDtl, name="postDtl"),
     path(r"post/write", views.post_write, name="post_write"),
-    path(r'api/', include(router.urls)),
-] 
+    path(r"api/", include(router.urls)),
+]
